0e000c2d40079956a4d72ac89dbe1e93064627e4
Gokul gopan <thisismyworkemailid@gmail.com>
2024-02-15 08:51:47 +0300

    sign-in page ui is completed and progressing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -12,8 +14,25 @@ mongoose.connect(process.env.MONGO).then(
 
 
 const app = express()
+
+app.use(express.json())
  
 
 app.listen(3000,() => {
     console.log("server running on port 3000vv ");
+});
+
+
+app.use('/api/users',userRoutes);
+app.use('/api/auth',authRoutes);
+
+
+app.use((err ,req, res , next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server error ';
+    res.status(statusCode).json({
+        sucess:false,
+        statusCode,
+        message,
+    });
 });
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import '../index.css';
+import {Link} from "react-router-dom"
+import { Button, Label, TextInput } from 'flowbite-react';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5' >
+
+        {/*left */}
+        <div className='flex-1'>
+        <Link to="/" className='  font-bold dark:text-white text-4xl' >
+        <span className='px-4 py-1 bg-gradient-to-r from-indigo-500 via-purple-400 to-pink-500 rounded-lg text-white '> A Little bit of </span>
+        everything
+        </Link>
+    
+    <p className='text-sm mt-5 font-bold'> Articulate anything living in your head rent free</p>
+    </div>
+      	{/*right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div  >
+              <Label value='Username' />
+              <TextInput  
+                  type='text'
+                  placeholder='Enter Username'
+                  id='username' />
+            </div>
+            <div  >
+              <Label value='Email' />
+              <TextInput  
+                  type='text'
+                  placeholder='Enter email'
+                  id='email' />
+            </div>
+            <div  >
+              <Label value='Password' />
+              <TextInput  
+                  type='password'
+                  placeholder='Enter Password'
+                  id='Password' />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>Sign Up</Button>
+          
+          </form>
+          <div className='flex gap-2 tex-sm mt-5'>
+            <span>Already have an account? </span>
+            <Link to='/sign-in' className='text-blue-500'> Sign in </Link>
+          </div>
+        
+        
+        </div>
+      
+      </div>
+    
+    </div>
+   
   )
 }
