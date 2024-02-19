17f60ee4bf02c90bb3973ace37b97cd3b62bedc3
Gokul gopan <thisismyworkemailid@gmail.com>
2024-02-19 09:18:25 +0300

    sign-in api route configured and tested working with insomnia

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,17 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js';
+import { signup , signin} from '../controllers/auth.controller.js';
 
 const router = express.Router()
 
 router.post('/signup',signup);
-
-router.get('/signup',(req,res) => {
-    res.json({message: 'signupapi is working'})
-})
-
-router.get('/test',(req,res) => {
-    res.json({message: 'api is working'})
-})
+router.post('/signin',signin)
 
  
 
